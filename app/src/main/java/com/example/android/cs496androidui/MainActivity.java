5c920a8618e52553353628b12aad3260a884ec6b
Cord Meados <meadosc@gmail.com>
2017-10-25 13:25:05 -0700

    Added buttons and activities. Horizontal view is linked. commit

--- app/src/main/java/com/example/android/cs496androidui/MainActivity.java
+++ app/src/main/java/com/example/android/cs496androidui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.cs496androidui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Horiztontal button click listener and onClick activity opener
+        Button button = (Button) findViewById(R.id.horizontal_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Open activity
+                Intent intent = new Intent(MainActivity.this, HorizontalViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
-
-
 }
